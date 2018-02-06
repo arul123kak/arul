30c53255b19b2ff7f3e61f491d9aaa358b8f08c3
arul123kak <35653183+arul123kak@users.noreply.github.com>
2018-02-06 16:18:27 +0530

    Create concept4.c

---
+++ concept4.c
@@ -0,0 +1,14 @@
+#incluude <stdio.h>
+int main()
+{
+    char b;
+    printf("Enter a character");
+    scanf("%b",&b);
+
+    if( (b>='a' && c<='z') || (b>='A' && b<='Z'))
+        printf("%b is an alphabet.",c);
+    else
+        printf("%b is not an alphabet.",c);
+
+    return 0;
+}
