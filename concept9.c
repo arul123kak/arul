8d4a710985e202c025c0ee61862e47499747d7ca
arul123kak <35653183+arul123kak@users.noreply.github.com>
2018-02-08 16:40:30 +0530

    Create concept9.c

---
+++ concept9.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a=36;
+int n,b,i;
+printf("\n enter the value of n");
+scanf("%d ",&n);
+printf("\n enter the vaue of b");
+scanf("%d",&b);
+if(i=1,i<n,n++)
+{
+printf("\n enter the value");
+scanf("%d",&a);
+if(i=1,i<b,b++);
+scanf("%d"&a);
+sum=a+sum(a);
+}
+}
